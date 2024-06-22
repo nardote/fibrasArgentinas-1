1c4182dbdeb328a59bdf513c1526b26dc1bd6e9d
Adrian Cavicchia <nardo.d@gmail.com>
2024-06-22 18:15:16 -0300

    test

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -14,7 +14,7 @@
     
     @vite('resources/js/admin.js')
 </body>
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
 </html>
